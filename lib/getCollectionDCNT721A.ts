6bd8d64aa6422f588bbd5517c4fecf03009cc5f9
Sweets Sweetman <sweetmantech@gmail.com>
2023-03-05 15:07:17 -0300

    I fix case for chillDrops contract addresses.

--- lib/getCollectionDCNT721A.ts
+++ lib/getCollectionDCNT721A.ts
@@ -5,28 +5,32 @@ import axios from "axios"
 import getErc721Drop from "./getErc721Drop"
 
 const getCollectionDCNT721A = async (contractAddress: string, provider: any) => {
-    const contract = new Contract(contractAddress, abi, provider)
-    const baseURI = await contract.baseURI();
-    const metadataURI = ipfsImage(baseURI)
-    const { data: metadata } = await axios.get(metadataURI)
-    const price = await contract.tokenPrice()
-    const maxSalePurchasePerAddress = await contract.maxTokenPurchase()
-    const totalSupply = await contract.totalSupply()
-    const maxSupply = await contract.MAX_TOKENS();
-    const publicSaleStart = await contract.saleStart()
-    const publicSaleEnd = await contract.saleEnd()
-    const dropParams = {
-        contractAddress,
-        metadata,
-        price,
-        maxSalePurchasePerAddress,
-        totalSupply,
-        maxSupply,
-        publicSaleStart: publicSaleStart.toString(),
-        publicSaleEnd: publicSaleEnd.toString()
+    try {
+        const contract = new Contract(contractAddress, abi, provider)
+        const baseURI = await contract.baseURI();
+        const metadataURI = ipfsImage(baseURI)
+        const { data: metadata } = await axios.get(metadataURI)
+        const price = await contract.tokenPrice()
+        const maxSalePurchasePerAddress = await contract.maxTokenPurchase()
+        const totalSupply = await contract.totalSupply()
+        const maxSupply = await contract.MAX_TOKENS();
+        const publicSaleStart = await contract.saleStart()
+        const publicSaleEnd = await contract.saleEnd()
+        const dropParams = {
+            contractAddress,
+            metadata,
+            price,
+            maxSalePurchasePerAddress,
+            totalSupply,
+            maxSupply,
+            publicSaleStart: publicSaleStart.toString(),
+            publicSaleEnd: publicSaleEnd.toString()
+        }
+        const erc721Drop = getErc721Drop(dropParams)
+        return erc721Drop
+    }catch(e) {
+        return false
     }
-    const erc721Drop = getErc721Drop(dropParams)
-    return erc721Drop
 }
 
 export default getCollectionDCNT721A
